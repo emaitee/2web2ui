329edfc589c965e1e6d3444b2b83ba7dd5c4d83c
Jose Zamora <jgzamora@terpmail.umd.edu>
2017-09-27 15:32:11 -0400

    moved regexs to helpers/regex

--- src/pages/credentials/helpers/validIpList.js
+++ src/pages/credentials/helpers/validIpList.js
@@ -1,6 +1,5 @@
 import { trim } from 'lodash';
-
-const ipRegex = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])(\/([0-9]|[1-2][0-9]|3[0-2]))?$/;
+import { ipRegex } from 'helpers/regex';
 
 export default function validIpList(value) {
   if (!value) {
