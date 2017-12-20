55dd0949d504857146cd9f30e320216b70ab9e92
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2017-12-19 20:46:56 -0500

    Delay report action test suggestions

--- src/actions/tests/delayReport.test.js
+++ src/actions/tests/delayReport.test.js
@@ -7,6 +7,7 @@ import moment from 'moment';
 jest.mock('../helpers/sparkpostApiRequest', () => jest.fn((a) => a));
 jest.mock('src/helpers/bounce');
 jest.mock('src/helpers/metrics');
+jest.mock('src/actions/metrics');
 jest.mock('src/actions/reportFilters');
 
 describe('Action Creator: Delay Report', () => {
@@ -14,31 +15,36 @@ describe('Action Creator: Delay Report', () => {
   const from = moment(new Date(1487076708000)).utc().format('YYYY-MM-DDTHH:MM');
   let dispatchMock;
   let getStateMock;
-  let fetchSpy;
+  let stateMock;
 
   beforeEach(() => {
-    fetchSpy = jest.spyOn(metricsActions, 'fetchDeliverability');
+    stateMock = {};
     metricsActions.fetchDeliverability = jest.fn(() => [{ count_accepted: 100, count_delay: 1 }]);
-
     metricsHelpers.getQueryFromOptions.mockImplementation(() => ({ from }));
 
     dispatchMock = jest.fn((a) => Promise.resolve(a));
-    getStateMock = jest.fn((a) => Promise.resolve(a));
+    getStateMock = jest.fn(() => stateMock);
   });
 
   afterEach(() => {
-    fetchSpy.mockRestore();
+    jest.resetAllMocks();
   });
 
   it('should dispatch delay reasons by domain and refresh table', async() => {
     const thunk = delayReport.loadDelayReasonsByDomain();
     await thunk(dispatchMock, getStateMock);
+
+    // can add same kind of expectations here on mock calls, just like bounce report action tests
+
     expect(dispatchMock.mock.calls).toMatchSnapshot();
   });
 
   it('should dispatch delay metrics and refresh the metrics', async() => {
     const thunk = delayReport.loadDelayMetrics();
     await thunk(dispatchMock, getStateMock);
+
+    // can add same kind of expectations here on mock calls, just like bounce report action tests
+
     expect(dispatchMock.mock.calls).toMatchSnapshot();
   });
 
