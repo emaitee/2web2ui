ac85e4a51cf7febc54e65718e4cb581fa59de4ee
Jose Zamora <jgzamora@terpmail.umd.edu>
2017-12-20 22:19:45 -0500

    Dynamic config + tests

--- src/components/routeWatch/RouteWatch.js
+++ src/components/routeWatch/RouteWatch.js
@@ -1,7 +1,8 @@
 import { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import config from 'src/config';
 
-class RouteWatch extends Component {
+export class RouteWatch extends Component {
   componentDidUpdate(prevProps) {
     if (prevProps.location.pathname !== this.props.location.pathname) {
       this.trackPageview();
@@ -10,7 +11,7 @@ class RouteWatch extends Component {
 
   trackPageview() {
     if (window.gtag) {
-      window.gtag('config', 'UA-111136819-2', { page_path: this.props.location.pathname });
+      window.gtag('config', config.gaTag, { page_path: this.props.location.pathname });
     }
   }
 
